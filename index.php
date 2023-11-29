3a3c8909d44c9693bec42d35f1fda96bf0832e38
DavideGila <davide.gila@gmail.com>
2023-11-29 15:14:04 +0100

    made a foreach to print on screen all its content

--- index.php
+++ index.php
@@ -2,7 +2,19 @@
 include __DIR__ . "/partials/header.php"
 ?>
 
-<main class="container"></main>
+<main class="container">
+    <div class="row">
+        <?php foreach ($hotels as $hotel) {?>
+            <div>
+                <?php echo $hotel["name"] ?>
+                <?php echo $hotel["description"] ?>
+                <?php echo $hotel["parking"] ?>
+                <?php echo $hotel["vote"] ?>
+                <?php echo $hotel["distance_to_center"] ?>
+            </div>
+        <?php } ?>
+    </div>
+</main>
 
 <?php
 include __DIR__ . "/partials/footer.php"
